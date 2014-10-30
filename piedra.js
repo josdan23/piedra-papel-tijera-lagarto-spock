5500aed0c7229fc820971d9ae19b28e7603fb384
josdan23 <josdan23@users.noreply.github.com>
2014-10-30 12:00:00 -0300

    Update piedra.js
    Se agrego la funcion "quienGana", que determina si el usuario gana o pierde, segun dos opciones de la máquina

--- piedra.js
+++ piedra.js
@@ -4,10 +4,26 @@ function aleatorio(minimo, maximo)
 	return numero = Math.floor( Math.random() * (maximo - minimo + 1) + minimo );
 }
 
-//función que muestra las opciones elegidas y el resultado de la partida
+//función que muestra la opcion elegida del usuario, de la maquina y el resultado de la partida
 function mensaje(tuEleccion, maquinaEleccion, resultado)
 {
 	alert("Tu: " + tuEleccion + "\nMáquina: " + maquinaEleccion + resultado);
+}
+
+//determina si ganas o pierdes de acuerdo a eleccion1 o eleccion2,
+//solo son necesarias dos opciones para saber quién gana
+//ej: si eliges "Piedra", solo puedes ganar si la máquina elige "Tijera" o "Lagarto"
+//caso contrario, pierdes, porque la máquina te gana con "Papel" o "Spock"
+function quienGana( eleccion1, eleccion2)
+{
+	if (opciones[opcionMaquina] == eleccion1 || opciones[opcionMaquina] == eleccion2)
+	{
+		mensaje(opciones[opcionUsuario], opciones[opcionMaquina], "\nGANASTE!");
+	}
+	else 
+	{
+		mensaje(opciones[opcionUsuario], opciones[opcionMaquina], "\nPERDISTE!");
+	}
 }
 
 // todas las opciones posibles
@@ -25,58 +41,23 @@ if ( opcionUsuario == opcionMaquina)  // si las elecciones son iguales
 }
 else if ( opciones[opcionUsuario] == "Piedra" ) // el usuario elige "Piedra"
 {
-	if ( (opciones[opcionMaquina] == "Tijera") || (opciones[opcionMaquina] == "Lagarto")) // la máquina elige "Tijera" o "Spock"
-	{
-		mensaje(opciones[opcionUsuario], opciones[opcionMaquina], "\nGANASTE!");
-	}
-	else // la maquina eligío "Spock" o "Papel"
-	{
-		mensaje(opciones[opcionUsuario], opciones[opcionMaquina], "\nPERDISTE!");
-	}
+	quienGana("Tijera", "Lagarto"); //si la maquina elige "Tijera" o "Lagarto" el usuario gana, caso contrario pierde
 }
 else if ( opciones[opcionUsuario] == "Papel") //El usuario elige "Piedra"
 {
-	if (opciones[opcionMaquina] == "Piedra" || opciones[opcionMaquina] == "Spock") // la máquina eligió "Piedra" o "Spock"
-	{
-		mensaje(opciones[opcionUsuario], opciones[opcionMaquina], "\nGANASTE!");
-	}
-	else // la máquina entonces eligío "Tijera" o "Lagarto"
-	{
-		mensaje(opciones[opcionUsuario], opciones[opcionMaquina], "\nPERDISTE!");
-	}
+	quienGana("Piedra", "Spock"); //si la maquina elige "Piedra" o "Spock" el usuario gana, caso contrario pierde
 }
 else if (opciones[opcionUsuario] == "Tijera") //El usuario elige "Tijera"
 {
-	if (opciones[opcionMaquina] == "Papel" || opciones[opcionMaquina] == "Lagarto") // la máquina eligío "Papel" o "Lagarto"
-	{
-		mensaje(opciones[opcionUsuario], opciones[opcionMaquina], "\nPERDISTE!");
-	}
-	else // la máquina eligío "Piedra" o "Spock"
-	{
-		mensaje(opciones[opcionUsuario], opciones[opcionMaquina], "\nGANASTE!");
-	}
+	quienGana("Papel", "Lagarto");//si la maquina elige "Papel" o "Lagarto" el usuario gana, caso contrario pierde
 }
 else if (opciones[opcionUsuario] == "Lagarto") //El usuario elige "Lagarto"
 {
-	if (opciones[opcionMaquina] == "Papel" || opciones[opcionMaquina] == "Spock") // la máquina eligió "Papel" o "Spock"
-	{
-		mensaje(opciones[opcionUsuario], opciones[opcionMaquina], "\nGANASTE!");
-	}
-	else // la máquina entonces eligío "Tijera" o "Piedra"
-	{
-		mensaje(opciones[opcionUsuario], opciones[opcionMaquina], "\nPERDISTE!");
-	}
+	quienGana("Papel", "Spock");//si la maquina elige "Papel" o "Spock" el usuario gana, caso contrario pierde
 }
 else if (opciones[opcionUsuario] == "Spock") //El usuario elige "Spock"
 {
-	if (opciones[opcionMaquina] == "Piedra" || opciones[opcionMaquina] == "Tijera") // la máquina eligió "Piedra" o "Tijera"
-	{
-		mensaje(opciones[opcionUsuario], opciones[opcionMaquina], "\nGANASTE!");
-	}
-	else // la máquina entonces eligío "Papel" o "Lagarto"
-	{
-		mensaje(opciones[opcionUsuario], opciones[opcionMaquina], "\nPERDISTE!");
-	}
+	quienGana("Piedra", "Tijera");//si la maquina elige "Piedra" o "Tijera" el usuario gana, caso contrario pierde
 }
 else // no elegiste una opción entre el 0 y 2.
 {
